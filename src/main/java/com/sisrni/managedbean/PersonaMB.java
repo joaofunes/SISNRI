de2dca88fbc0524a64ecb67b9cc6c75f1027be2b
joaofunes <joao.hfunes@gmail.com>
2016-10-05 23:17:53 -0600

    actualizacion creacion persona

--- src/main/java/com/sisrni/managedbean/PersonaMB.java
+++ src/main/java/com/sisrni/managedbean/PersonaMB.java
@@ -8,12 +8,17 @@ package com.sisrni.managedbean;
 import com.sisrni.managedbean.generic.GenericManagedBean;
 import com.sisrni.model.Organismo;
 import com.sisrni.model.Persona;
+import com.sisrni.model.TipoPersona;
 import com.sisrni.model.Unidad;
 import com.sisrni.service.OrganismoService;
+import com.sisrni.service.PersonaService;
+import com.sisrni.service.TipoPersonaService;
 import com.sisrni.service.UnidadService;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -32,10 +37,10 @@ public class PersonaMB implements Serializable{
     
     private List<Organismo> listadoOrganismo;
     private List<Unidad> listadoUnidad;
-     private List<String> selectedTipoPersona;
+    private List<String> selectedTipoPersona;
     
     private Persona persona;
-    
+    private TipoPersona tipoPersona;
     
     @Autowired
     @Qualifier(value = "organismoService")
@@ -44,6 +49,16 @@ public class PersonaMB implements Serializable{
     @Autowired
     @Qualifier(value = "unidadService")
     private UnidadService unidadService;
+    
+    @Autowired
+    @Qualifier(value = "personaService")
+    private PersonaService personaService;
+    
+    @Autowired
+    @Qualifier(value = "tipoPersonaService")
+    private TipoPersonaService tipoPersonaService;
+    
+    
     
     
     //declaracion de listas
@@ -71,6 +86,24 @@ public class PersonaMB implements Serializable{
          }
     }
 
+    /**
+     * Metodo para almacenar una nueva persona
+     */ 
+    public void guardar(){
+        try {
+            String msg = "Persona Almacenado Exitosamente!";    
+            for(String us:selectedTipoPersona){
+                tipoPersona = tipoPersonaService.getTipoPersonaByNombre(us);
+            }            
+            persona.setIdTipoPersona(tipoPersona);
+            personaService.save(persona);
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Guardado", msg));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+     
     public List<Organismo> getListadoOrganismo() {
         return listadoOrganismo;
     }
@@ -101,6 +134,22 @@ public class PersonaMB implements Serializable{
 
     public void setSelectedTipoPersona(List<String> selectedTipoPersona) {
         this.selectedTipoPersona = selectedTipoPersona;
+    }
+
+    public PersonaService getPersonaService() {
+        return personaService;
+    }
+
+    public void setPersonaService(PersonaService personaService) {
+        this.personaService = personaService;
+    }
+
+    public TipoPersona getTipoPersona() {
+        return tipoPersona;
+    }
+
+    public void setTipoPersona(TipoPersona tipoPersona) {
+        this.tipoPersona = tipoPersona;
     }
 
    
--- src/main/java/com/sisrni/service/TipoPersonaService.java
+++ src/main/java/com/sisrni/service/TipoPersonaService.java
@@ -5,10 +5,9 @@
  */
 package com.sisrni.service;
 
-
-import com.sisrni.dao.TipoOrganismoDao;
+import com.sisrni.dao.TipoPersonaDao;
 import com.sisrni.dao.generic.GenericDao;
-import com.sisrni.model.TipoOrganismo;
+import com.sisrni.model.TipoPersona;
 import com.sisrni.service.generic.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,16 +16,20 @@ import org.springframework.stereotype.Service;
  *
  * @author Joao
  */
-@Service(value = "tipoOrganismoService")
-public class TipoOrganismoService extends GenericService<TipoOrganismo, Integer>  {
+
+@Service(value = "tipoPersonaService")
+public class TipoPersonaService extends GenericService<TipoPersona, Integer>  {
 
     @Autowired
-    private TipoOrganismoDao tipoOrganismoDao;
+    private TipoPersonaDao tipoPersonaDao;
+   
     @Override
-    public GenericDao<TipoOrganismo, Integer> getDao() {
-      return tipoOrganismoDao;
+    public GenericDao<TipoPersona, Integer> getDao() {
+      return tipoPersonaDao;
     }
     
-    
+    public TipoPersona getTipoPersonaByNombre(String name){
+     return tipoPersonaDao.getTipoPersonaByNombre(name);
+    }
     
 }
