abbd23c06b8926c96dff3b7518ecb4943c90e297
angel r <angel r>
2016-10-22 20:33:06 -0600

    avance modificar Proyecto

--- src/main/java/com/sisrni/managedbean/ProyectoMB.java
+++ src/main/java/com/sisrni/managedbean/ProyectoMB.java
@@ -5,12 +5,16 @@
  */
 package com.sisrni.managedbean;
 
+import com.sisrni.model.AreaConocimiento;
 import com.sisrni.model.Facultad;
 import com.sisrni.model.Organismo;
 import com.sisrni.model.Persona;
+import com.sisrni.model.Telefono;
+import com.sisrni.service.AreaConocimientoService;
 import com.sisrni.service.FacultadService;
 import com.sisrni.service.OrganismoService;
 import com.sisrni.service.PersonaService;
+import com.sisrni.service.TelefonoService;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -27,6 +31,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 @Named(value = "proyectoMB")
 @ViewScoped
 public class ProyectoMB {
+    
+    private static final String FIJO ="FIJO";  
+    private static final String FAX ="FAX";
+    private String [] areaConocimientoSelected;
+    
    /*Variables*/
     private List<Facultad> listFacultad;
     private List<SelectItem> listFacultadItem;
@@ -35,10 +44,24 @@ public class ProyectoMB {
     private List<SelectItem> listOrganismosItem;
     
     private List<Persona> listInterno;
-    private List<SelectItem> listInternoItem;
+    private List<Telefono> listTelefonoInterno;
     
     private List<Persona> listAsistenteInterno;
-    private List<SelectItem> listAsistenteInternoItem;
+    private List<Telefono> listTelefonoAsistenteInterno;
+    
+    private List<AreaConocimiento> listAreaConocimiento;
+    private List<SelectItem> listAreaConocimientoItem;
+    
+    private Persona persona;
+    private Persona personaCoordinador;
+    private Persona personaAsistente;
+    
+    private Telefono telFijoInterno;
+    private Telefono faxInterno;
+    
+    private Telefono telFijoAsistente;
+    private Telefono faxAsistente;
+    
     
     @Autowired
     @Qualifier(value = "facultadService")
@@ -51,6 +74,14 @@ public class ProyectoMB {
     @Autowired
     @Qualifier(value = "personaService")
     private PersonaService personaService;
+    
+    @Autowired
+    @Qualifier(value ="telefonoService")
+    private TelefonoService telefonoService;
+    
+    @Autowired
+    @Qualifier(value ="areaConocimientoService")
+    private AreaConocimientoService areaConocimientoService;
     
 
     /**
@@ -65,11 +96,56 @@ public class ProyectoMB {
     
     @PostConstruct
     public void init(){
+      personaCoordinador = new Persona();  
+      personaAsistente = new Persona();
       listFacultad = facultadService.findAll();
       listOrganismos = organismoService.findAll();
       listInterno = personaService.findAll();
       listAsistenteInterno =personaService.findAll();
+      listAreaConocimiento = areaConocimientoService.findAll();
+      
+      telFijoInterno = new Telefono();
+      faxInterno = new Telefono();
     }
+    
+    
+    public void onchangeCoordinador(){
+        try{
+            listTelefonoInterno = telefonoService.getTelefonosByPersona(personaCoordinador);
+            
+            for(Telefono tlfx : listTelefonoInterno){
+                if(tlfx.getIdTipoTelefono().getNombre().equals(FIJO)){
+                    telFijoInterno =tlfx;
+                }
+                if(tlfx.getIdTipoTelefono().getNombre().equals(FAX)){
+                    faxInterno = tlfx;
+                }
+            }
+        }catch(Exception e){
+            
+        }
+    }
+    
+     public void onchangeAsistenteCoordinador(){
+        try{
+            listTelefonoAsistenteInterno = telefonoService.getTelefonosByPersona(personaAsistente);
+            
+            for(Telefono tlfx : listTelefonoAsistenteInterno){
+                if(tlfx.getIdTipoTelefono().getNombre().equals(FIJO)){
+                    telFijoAsistente =tlfx;
+                }
+                if(tlfx.getIdTipoTelefono().getNombre().equals(FAX)){
+                    faxAsistente = tlfx;
+                }
+            }
+        }catch(Exception e){
+            
+        }
+    }
+    
+    
+    
+    
 
     public List<Facultad> getListFacultad() {
         return listFacultad;
@@ -127,21 +203,7 @@ public class ProyectoMB {
         this.listInterno = listInterno;
     }
 
-    public List<SelectItem> getListInternoItem() {
-         this.listInternoItem = new ArrayList<SelectItem>();
-         listInternoItem.clear();
-        
-        for(Persona p :listInterno ){
-          SelectItem internoItem = new SelectItem(p.getIdPersona(),p.getNombrePersona() + " "+ p.getApellidoPersona());
-          this.listInternoItem.add(internoItem);
-    }
-        return listInternoItem;
-    }
-
-    public void setListInternoItem(List<SelectItem> listInternoItem) {
-        this.listInternoItem = listInternoItem;
-    }
-
+ 
     public List<Persona> getListAsistenteInterno() {
         return listAsistenteInterno;
     }
@@ -150,20 +212,102 @@ public class ProyectoMB {
         this.listAsistenteInterno = listAsistenteInterno;
     }
 
-    public List<SelectItem> getListAsistenteInternoItem() {
-         this.listAsistenteInternoItem = new ArrayList<SelectItem>();
-         listAsistenteInternoItem.clear();
+    public Persona getPersonaCoordinador() {
         
-        for(Persona pa :listAsistenteInterno ){
-          SelectItem asistenteInternoItem = new SelectItem(pa.getIdPersona(),pa.getNombrePersona() + " " + pa.getApellidoPersona());
-          this.listAsistenteInternoItem.add(asistenteInternoItem);
-    }
-        
-        return listAsistenteInternoItem;
+        return personaCoordinador;
     }
 
-    public void setListAsistenteInternoItem(List<SelectItem> listAsistenteInternoItem) {
-        this.listAsistenteInternoItem = listAsistenteInternoItem;
+    public void setPersonaCoordinador(Persona personaCoordinador) {
+        this.personaCoordinador = personaCoordinador;
+    }
+
+    public Persona getPersonaAsistente() {
+        return personaAsistente;
+    }
+
+    public void setPersonaAsistente(Persona personaAsistente) {
+        this.personaAsistente = personaAsistente;
+    }
+    
+    
+    
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public List<Telefono> getListTelefonoInterno() {
+        return listTelefonoInterno;
+    }
+
+    public void setListTelefonoInterno(List<Telefono> listTelefonoInterno) {
+        this.listTelefonoInterno = listTelefonoInterno;
+    }
+
+    public Telefono getTelFijoInterno() {
+        return telFijoInterno;
+    }
+
+    public void setTelFijoInterno(Telefono telFijoInterno) {
+        this.telFijoInterno = telFijoInterno;
+    }
+
+    public Telefono getFaxInterno() {
+        return faxInterno;
+    }
+
+    public void setFaxInterno(Telefono faxInterno) {
+        this.faxInterno = faxInterno;
+    }
+
+    public Telefono getTelFijoAsistente() {
+        return telFijoAsistente;
+    }
+
+    public void setTelFijoAsistente(Telefono telFijoAsistente) {
+        this.telFijoAsistente = telFijoAsistente;
+    }
+
+    public Telefono getFaxAsistente() {
+        return faxAsistente;
+    }
+
+    public void setFaxAsistente(Telefono faxAsistente) {
+        this.faxAsistente = faxAsistente;
+    }
+
+    public List<AreaConocimiento> getListAreaConocimiento() {
+         this.listAreaConocimientoItem = new ArrayList<SelectItem>();
+         listAreaConocimientoItem.clear();
+        
+        for(AreaConocimiento a :listAreaConocimiento ){
+          SelectItem areaConocimientoItem = new SelectItem(a.getIdAreaConocimiento(),a.getNombreArea());
+          this.listOrganismosItem.add(areaConocimientoItem);
+    }
+        return listAreaConocimiento;
+    }
+
+    public void setListAreaConocimiento(List<AreaConocimiento> listAreaConocimiento) {
+        this.listAreaConocimiento = listAreaConocimiento;
+    }
+
+    public List<SelectItem> getListAreaConocimientoItem() {
+        return listAreaConocimientoItem;
+    }
+
+    public void setListAreaConocimientoItem(List<SelectItem> listAreaConocimientoItem) {
+        this.listAreaConocimientoItem = listAreaConocimientoItem;
+    }
+
+    public String[] getAreaConocimientoSelected() {
+        return areaConocimientoSelected;
+    }
+
+    public void setAreaConocimientoSelected(String[] areaConocimientoSelected) {
+        this.areaConocimientoSelected = areaConocimientoSelected;
     }
 
     
