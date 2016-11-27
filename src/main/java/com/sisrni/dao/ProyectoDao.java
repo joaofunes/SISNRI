ab83711901a8c9e9567d29d0c15b4025066ded3c
Henry Cortez <henrycortez2509@gmail.com>
2016-11-26 19:00:08 -0600

    mapa2

--- src/main/java/com/sisrni/dao/ProyectoDao.java
+++ src/main/java/com/sisrni/dao/ProyectoDao.java
@@ -7,6 +7,12 @@ package com.sisrni.dao;
 
 import com.sisrni.dao.generic.GenericDao;
 import com.sisrni.model.Proyecto;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.Hibernate;
+
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,5 +21,40 @@ import org.springframework.stereotype.Repository;
  */
 @Repository(value = "proyectoDao")
 public class ProyectoDao extends GenericDao<Proyecto, Integer> {
-    
+
+    public List<Proyecto> getProjectListToCharts(String codigoPais, int idTipoProyecto, String desde, String hasta) {
+        String query = "SELECT pr FROM Proyecto as pr WHERE 1=1";
+        if (idTipoProyecto > 0) {
+            query = query + " and pr.idTipoProyecto.idTipoProyecto=:idTipoProyecto";
+        }
+        if (desde != null && hasta != null) {
+            query = query + " and to_char(pr.fechaGestion,'YYYY') BETWEEN :desde and :hasta";
+        }
+
+        Query q = getSessionFactory().getCurrentSession().createQuery(query);
+        q.setParameter("idTipoProyecto", idTipoProyecto);
+        q.setParameter("desde", desde);
+        q.setParameter("hasta", hasta);
+        return q.list();
+    }
+
+    public List<Proyecto> getProjectListToChartsByYear(String codigoPais, int idTipoProyecto, Date year) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(year);
+        int yeard = cal.get(Calendar.YEAR);
+        String yearString = "" + yeard;
+
+        String query = "SELECT pr FROM Proyecto as pr WHERE 1=1";
+        if (idTipoProyecto > 0) {
+            query = query + " and pr.idTipoProyecto.idTipoProyecto=:idTipoProyecto";
+        }
+
+        query = query + "to_chart(pr.fechaGestion,'YYYY')= :yearString";
+
+        Query q = getSessionFactory().getCurrentSession().createQuery(query);
+        q.setParameter("idTipoProyecto", idTipoProyecto);
+        q.setParameter("yearString", yearString);
+
+        return q.list();
+    }
 }
--- src/main/java/com/sisrni/service/ProyectoService.java
+++ src/main/java/com/sisrni/service/ProyectoService.java
@@ -9,6 +9,8 @@ import com.sisrni.dao.ProyectoDao;
 import com.sisrni.dao.generic.GenericDao;
 import com.sisrni.model.Proyecto;
 import com.sisrni.service.generic.GenericService;
+import java.util.Date;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,15 +18,23 @@ import org.springframework.stereotype.Service;
  *
  * @author Joao
  */
-
 @Service(value = "proyectoService")
 public class ProyectoService extends GenericService<Proyecto, Integer> {
 
     @Autowired
     private ProyectoDao proyectoDao;
+
     @Override
     public GenericDao<Proyecto, Integer> getDao() {
-       return proyectoDao;
+        return proyectoDao;
     }
-    
+
+    public List<Proyecto> getProjectListToCharts(String codigoPais, int idTipoProyecto, String desde, String hasta) {
+        return proyectoDao.getProjectListToCharts(codigoPais, idTipoProyecto, desde, hasta);
+    }
+
+    public List<Proyecto> getProjectListToChartsByYear(String codigoPais, int idTipoProyecto, Date year) {
+        return proyectoDao.getProjectListToChartsByYear(codigoPais, idTipoProyecto, year);
+    }
+
 }
