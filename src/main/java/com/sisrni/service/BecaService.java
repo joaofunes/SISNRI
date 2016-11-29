6833650fbb380b8757c8a4201381db26faa1898f
lilian.cisneros <liliancisneros29@gmail.com>
2016-11-28 22:28:57 -0600

    incluido beca, datos restantes

---
+++ src/main/java/com/sisrni/service/BecaService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sisrni.service;
+
+import com.sisrni.dao.BecaDao;
+import com.sisrni.dao.generic.GenericDao;
+import com.sisrni.model.Beca;
+import com.sisrni.service.generic.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Lillian
+ */
+
+@Service(value = "becaService")
+public class BecaService extends GenericService<Beca, Integer> {
+
+    @Autowired
+    private BecaDao becaDao;
+    @Override
+    public GenericDao<Beca, Integer> getDao() {
+       return becaDao;
+    }
+    
+}
