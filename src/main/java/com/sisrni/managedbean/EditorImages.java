c99cc7a26030f6ee805062580e7bd63fa4643713
Henry Cortez <henrycortez2509@gmail.com>
2016-12-11 15:14:08 -0600

    finalizado estilo

--- src/main/java/com/sisrni/managedbean/EditorImages.java
+++ src/main/java/com/sisrni/managedbean/EditorImages.java
@@ -5,9 +5,20 @@
  */
 package com.sisrni.managedbean;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.FilenameUtils;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.FileUploadEvent;
 
 /**
  *
@@ -33,4 +44,52 @@ public class EditorImages implements Serializable {
         this.text = text;
     }
 
+    public void uploadListener(FileUploadEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+
+        String fileName = FilenameUtils.getName(event.getFile().getFileName());
+        String fileNamePrefix = FilenameUtils.getBaseName(fileName) + "_";
+        String fileNameSuffix = "." + FilenameUtils.getExtension(fileName);
+
+        File uploadFolder = new File("/var/webapp/uploads");
+
+        try {
+            File result = File.createTempFile(fileNamePrefix, fileNameSuffix, uploadFolder);
+
+            FileOutputStream fileOutputStream = new FileOutputStream(result);
+            byte[] buffer = new byte[1024];
+            int bulk;
+
+            InputStream inputStream = event.getFile().getInputstream();
+            while (true) {
+                bulk = inputStream.read(buffer);
+                if (bulk < 0) {
+                    break;
+                }
+
+                fileOutputStream.write(buffer, 0, bulk);
+                fileOutputStream.flush();
+            }
+
+            fileOutputStream.close();
+            inputStream.close();
+
+            String value = FacesContext.getCurrentInstance().
+                    getExternalContext().getRequestParameterMap().get("editor_input");
+            setText(value + "<img src=\"/rais/uploads/" + result.getName() + "\" />");
+
+            RequestContext.getCurrentInstance().update("editor_input");
+
+            FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            FacesMessage error = new FacesMessage("The files were not uploaded!");
+            FacesContext.getCurrentInstance().addMessage(null, error);
+        }
+
+    }
 }
