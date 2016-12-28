910bea537ac1d73aa91caeb8fc081e800eebb019
Henry Cortez <henrycortez2509@gmail.com>
2016-12-27 19:41:34 -0600

    comente algunas cosas revisar si quiren jajaja

---
+++ src/main/java/com/sisrni/service/ProgramaBecaService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sisrni.service;
+
+import com.sisrni.dao.ProgramaBecaDao;
+import com.sisrni.dao.generic.GenericDao;
+import com.sisrni.model.ProgramaBeca;
+import com.sisrni.service.generic.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Cortez
+ */
+@Service(value = "programaBecaService")
+public class ProgramaBecaService extends GenericService<ProgramaBeca, Integer> {
+
+    @Autowired
+    ProgramaBecaDao programaBecaDoa;
+
+    @Override
+    public GenericDao<ProgramaBeca, Integer> getDao() {
+        return programaBecaDoa;
+    }
+
+}
