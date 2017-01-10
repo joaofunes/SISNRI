0d4e7afc179b40e4c67eb2eaedf1dd71ee7a5eff
joao.hfunes@gmail.com <joao.hfunes@gmail.com>
2017-01-09 23:21:36 -0600

    reporte

---
+++ src/main/java/com/sisrni/managedbean/PersonaReportMB.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sisrni.managedbean;
+
+import com.sisrni.jasper.Reporte;
+import com.sisrni.model.Persona;
+import com.sisrni.pojo.rpt.RptPersona;
+import com.sisrni.service.PersonaService;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+import org.primefaces.context.RequestContext;
+import org.primefaces.model.LazyDataModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ *
+ * @author Joao
+ */
+@Named("personaReportMB")
+@Scope(WebApplicationContext.SCOPE_SESSION)
+public class PersonaReportMB {
+    
+    @Autowired
+    @Qualifier(value = "personaService")
+    private PersonaService personaService;
+    
+    private String reportName;
+    @PostConstruct
+    public void init() {
+        
+    }
+
+    
+    public void llenarReporte() {
+        List<RptPersona> list = new ArrayList<RptPersona>();
+
+        List<Persona> comparativoReparacionesDos = personaService.findAll();
+        RptPersona prueba = new RptPersona();
+
+        for (Persona item : comparativoReparacionesDos) {
+            prueba = new RptPersona();
+            prueba.setApellido(item.getApellidoPersona());
+            prueba.setNombre(item.getNombrePersona());
+            prueba.setEmail(item.getEmailPersona());
+            list.add(prueba);
+        }
+
+        print(list);
+    }
+
+    public void print(List<RptPersona> list) {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        HttpServletResponse response = (HttpServletResponse) context.getResponse();
+        Reporte reporte = new Reporte("compgastosrep", "rpt_comparativo_gasto_reparaciones", request);
+        reporte.setDataSource(new JRBeanCollectionDataSource(new HashSet<RptPersona>(list)));
+        reporte.addParameter("fechaInicial", new Date());
+        reporte.addParameter("fechaFinal", new Date());
+        reporte.addParameter("usuario", "ADM");
+        reporte.addParameter("equipox", "Equipo 1");
+        reporte.addParameter("equipoy", "Equipo 2");
+        reporte.setReportInSession(request, response);
+        reportName = reporte.getNombreLogico();
+        RequestContext.getCurrentInstance().addCallbackParam("reportName", reportName);
+    }
+
+    public String getReportName() {
+        return reportName;
+    }
+
+    public void setReportName(String reportName) {
+        this.reportName = reportName;
+    }
+
+}
---
+++ src/main/java/com/sisrni/pojo/rpt/RptPersona.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sisrni.pojo.rpt;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Joao
+ */
+public class RptPersona implements Serializable{
+    
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
