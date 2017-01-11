1be2f0ba0118b46269bf4135e6e803734ceb0425
Luis <Luis@Marrokings7>
2017-01-11 12:43:25 -0600

    Crud CategoriaNoticia views/CategoriaNoticia/categoriaNoticia.xhtml

---
+++ src/main/java/com/sisrni/managedbean/CategoriaNoticiaMB.java
@@ -0,0 +1,209 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sisrni.managedbean;
+
+
+import com.sisrni.model.CategoriaNoticia;
+import com.sisrni.service.CategoriaNoticiaService;
+import com.sisrni.utils.JsfUtil;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.primefaces.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+/**
+ *
+ * @author Luis
+ */
+@Named(value = "categoriaNoticiaMB")
+@RequestScoped
+public class CategoriaNoticiaMB{
+      /*Para Errores*/
+    private final static Log log = LogFactory.getLog(CategoriaNoticiaMB.class);
+
+    
+    /*Variables */
+    private CategoriaNoticia categoriaNoticia;
+    private List<CategoriaNoticia> listCategoriaNoticia;
+    private boolean actualizar;
+    
+    
+    
+    @Autowired
+    @Qualifier(value = "categoriaNoticiaService")
+    private CategoriaNoticiaService categoriaNoticiaService;
+    
+    
+    /*Constructor*/
+    public CategoriaNoticiaMB(){
+        
+    }
+    
+    
+    /*Post Constructor*/
+    @PostConstruct
+    public void init(){
+        cargarCategoriaNoticia();
+    }
+    
+    /** 
+     * Metodo que crea instancias de 'CategoriaNoticia' y 'Organismo' y almacena
+     * en una Lista todas las instancias de 'Organismo' que se encuentra en la
+     * tabla respectiva
+     */
+    
+    public void cargarCategoriaNoticia(){
+        categoriaNoticia = new CategoriaNoticia();
+        listCategoriaNoticia = categoriaNoticiaService.findAll();
+        actualizar = false;
+    }
+    
+    
+    /** 
+     * Metodo para guardar una instancia de 'CategoriaNoticia' en la tabla 
+     * correspondiente de la base de datos
+     */
+    public void guardarCategoriaNoticia(){
+        String msg ="CategoriaNoticia Almacenado Exitosamente!";
+        try{
+           categoriaNoticia.setIdCategoria(Integer.MIN_VALUE);
+           categoriaNoticiaService.save(categoriaNoticia);
+          FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Guardado!!", msg));
+           
+        }catch(Exception e){
+            JsfUtil.addErrorMessage("Error al Guardar CategoriaNoticia!");
+            e.printStackTrace();
+        }
+        cargarCategoriaNoticia();
+    }
+    
+    /**
+     * Metodo que se ocupa de precargar la instancia de 'CategoriaNoticia' a ser actualizada
+     * @param categoriaNoticia
+     */
+    public void preActualizarCategoriaNoticia(CategoriaNoticia categoriaNoticia){
+       try{ 
+        actualizar = true;
+        this.categoriaNoticia = categoriaNoticia;
+       }catch(Exception e){
+           System.out.println(e.getMessage());
+       }
+    }
+    
+    /**
+     * Metodo para actualizar las propiedades de la instancia de 'CategoriaNoticia'
+     * seleccionada
+     */
+    public void actualizarCategoriaNoticia(){
+        String msg ="CategoriaNoticia Actualizado Exitosamente!";
+        try{
+            //actualizando la instancia
+            categoriaNoticiaService.merge(categoriaNoticia);
+            actualizar = false;
+            cancelarCategoriaNoticia(); 
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Actualizacion!!", msg));
+        }catch(Exception e){
+            JsfUtil.addErrorMessage("Error al Actualizar CategoriaNoticia");
+            e.printStackTrace();
+        }
+        cargarCategoriaNoticia(); 
+    }
+    
+    
+    /**
+     * Metodo  de Pre- borrado, que obtiene una instancia de la Entity a Borrar
+     * y despliega un 'p:dialog' donde se solicita la confirmacion de la 
+     * operacion de borrado
+     * @param categoriaNoticia
+     */
+    public void preBorradoCategoriaNoticia(CategoriaNoticia categoriaNoticia){
+        //guardando en 'categoriaNoticia' la instancia de 'CategoriaNoticia' que se recibe como argumento
+        this.categoriaNoticia = categoriaNoticia;
+        RequestContext context = RequestContext.getCurrentInstance();
+        //Desplegando el 'Dialog'
+        context.execute("PF('confirmDeleteCategoriaNoticiaDlg').show();");
+    }
+    
+    /**
+     * Metodo que borra una instancia de 'CategoriaNoticia' de la Base de datos
+     */
+    public void borrarCategoriaNoticia(){ 
+        String msg ="CategoriaNoticia Eliminado Exitosamente!";
+        try{
+            //Borrando la instancia de categoriaNoticia
+            categoriaNoticiaService.delete(categoriaNoticia);
+            cargarCategoriaNoticia();
+            RequestContext context = RequestContext.getCurrentInstance();
+            context.execute("PF('confirmDeleteCategoriaNoticiaDlg').hide();"); 
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Eliminado!!", msg));
+        }catch(Exception e){
+            JsfUtil.addErrorMessage("Error al Eliminar CategoriaNoticia!");
+            e.printStackTrace();
+        }finally{
+            actualizar = false;
+        }
+        
+        
+    }
+    
+    
+    /**
+     * Metodo que se encarga de limpiar el formulario de creacion y 
+     * actualizacion de CategoriaNoticia
+     */
+    public void cancelarCategoriaNoticia(){
+        String msg ="CategoriaNoticia cancelado";
+        try{
+        categoriaNoticia = null;
+        categoriaNoticia = new CategoriaNoticia();
+        RequestContext.getCurrentInstance().reset(":formCategoriaNoticia");
+        JsfUtil.addSuccessMessage(msg);
+        }catch(Exception e){
+             System.out.println(e.getMessage());
+        }
+      cargarCategoriaNoticia();
+    }
+            
+    
+
+    
+    /*Getters y Setters*/
+
+    public CategoriaNoticia getCategoriaNoticia() {
+        return categoriaNoticia;
+    }
+
+    public void setCategoriaNoticia(CategoriaNoticia categoriaNoticia) {
+        this.categoriaNoticia = categoriaNoticia;
+    }
+
+    public List<CategoriaNoticia> getListCategoriaNoticia() {
+        return listCategoriaNoticia;
+    }
+
+    public void setListCategoriaNoticia(List<CategoriaNoticia> listCategoriaNoticia) {
+        this.listCategoriaNoticia = listCategoriaNoticia;
+    }
+
+    public boolean isActualizar() {
+        return actualizar;
+    }
+
+    public void setActualizar(boolean actualizar) {
+        this.actualizar = actualizar;
+    }
+    
+   
+    
+}
