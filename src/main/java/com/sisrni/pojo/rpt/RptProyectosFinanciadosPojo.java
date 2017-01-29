90e9c4943844923ea4e6fd21b244f74ead1c6c88
lilian.cisneros <liliancisneros29@gmail.com>
2017-01-28 18:12:37 -0600

    reportes de proyectos y consulta de proyecto

---
+++ src/main/java/com/sisrni/pojo/rpt/RptProyectosFinanciadosPojo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sisrni.pojo.rpt;
+
+/**
+ *
+ * @author Joao
+ */
+public class RptProyectosFinanciadosPojo{
+    
+    private Integer anioGestion;
+    private double suma;
+
+    public Integer getAnioGestion() {
+        return anioGestion;
+    }
+
+    public void setAnioGestion(Integer anioGestion) {
+        this.anioGestion = anioGestion;
+    }
+
+    public double getSuma() {
+        return suma;
+    }
+
+    public void setSuma(double suma) {
+        this.suma = suma;
+    }
+    
+    
+
+
+}
