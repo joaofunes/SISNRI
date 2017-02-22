dbe759f1296b6b2016c94d3653f4dd80dbd86fec
Henry Cortez <henrycortez2509@gmail.com>
2017-02-21 20:39:54 -0600

    cambios de cotto

--- src/main/java/com/sisrni/model/Beca.java
+++ src/main/java/com/sisrni/model/Beca.java
@@ -7,6 +7,7 @@ package com.sisrni.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Basic;
@@ -74,7 +75,7 @@ public class Beca implements Serializable {
     @OneToMany(mappedBy = "idBeca")
     private List<Documento> documentoList;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "beca")
-    private List<PersonaBeca> personaBecaList;
+    private List<PersonaBeca> personaBecaList=new ArrayList<PersonaBeca>();
     @JoinColumn(name = "ID_UNIVERSIDAD", referencedColumnName = "ID_ORGANISMO")
     @ManyToOne
     private Organismo idUniversidad;
