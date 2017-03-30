80ef2ee4ebedc2215312766b133060d220f1ad96
angel r <angel r>
2017-03-30 12:17:13 -0600

    actualizacion

--- src/main/java/com/sisrni/dao/PersonaMovilidadDao.java
+++ src/main/java/com/sisrni/dao/PersonaMovilidadDao.java
@@ -1,7 +1,9 @@
 package com.sisrni.dao;
 
 import com.sisrni.dao.generic.GenericDao;
+import com.sisrni.model.Movilidad;
 import com.sisrni.model.PersonaMovilidad;
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +12,19 @@ import org.springframework.stereotype.Repository;
  */
 @Repository(value = "personamovilidadDao")
 public class PersonaMovilidadDao extends GenericDao<PersonaMovilidad, Integer>{
+    /**
+     * Metodo para eliminar relacion entre Movilidad y Persona
+     * @param mov 
+     */
+    public void eliminarIntemediaPersonaMovilidad(Movilidad mov){
+        try{
+            String query = "DELETE FROM PERSONA_MOVILIDAD WHERE ID_MOVILIDAD = "+ mov.getIdMovilidad();
+            Query q = getSessionFactory().getCurrentSession().createSQLQuery(query);
+            q.executeUpdate();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
     
 }
