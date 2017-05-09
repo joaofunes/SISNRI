e5b4e9420844358256ed23869dec2088c45bd20b
Henry Cortez <henrycortez2509@gmail.com>
2017-05-09 12:56:05 -0600

    token fb

--- src/main/java/com/sisrni/managedbean/NoticiaMB.java
+++ src/main/java/com/sisrni/managedbean/NoticiaMB.java
@@ -68,7 +68,7 @@ public class NoticiaMB implements Serializable {
     private Boolean renderFbButton;
     private File fileForFb;
     private FileInputStream fileToPublish;
-    private static final String tokenFb = "EAACEdEose0cBAEmrmuue8oR3o6ieLTEjXh8epMtnPaX5uoVbLTC8mnZBbGU516wKhk3zCZBul98gCyEUcJxdsf8pMWlcJZC6UfpcjXXmrZBXPjQfpo8AlZAsJLVwHLK8a5is9uv1lYsPSohsZAqfifx9IenbvZAcRl6ZCl5TSpbrGtTiSOwnvuWZCfMiZARhDJMYkZD";
+    private static final String tokenFb = "EAAEF5ZAzqmoQBAJCVXVxgsT56JE62dZB8mVXgSL4mv3yewNZBTZAtLzHnMHBjZBiSOvfDOScuEs3ZCEHZBIqQAytLjB88w7ARR8RJMf1yDF2jwKbYZB8G1y3bhixgvs2jJeIyMqol1JkqlN1BAdVAR9E19XNUSNDbXZAIftGi2jZCJgfBjPm8ZC1tPG";
 
     @Autowired
     @ManagedProperty("#{globalCounterView}")
